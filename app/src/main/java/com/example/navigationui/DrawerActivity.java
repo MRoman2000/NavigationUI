32ee01b83a04d7a31a2c6faa4628c10775a69afc
RomanMadzey <madzey71@gmail.com>
2023-11-25 20:00:26 +0100

    Commit

---
+++ app/src/main/java/com/example/navigationui/DrawerActivity.java
@@ -0,0 +1,35 @@
+package com.example.navigationui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.example.navigationui.databinding.ActivityDrawerBinding;
+
+public class DrawerActivity extends AppCompatActivity {
+    ActivityDrawerBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((binding = ActivityDrawerBinding.inflate(getLayoutInflater())).getRoot());
+
+
+        setSupportActionBar(binding.toolbar);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                // Top-level destinations:
+                R.id.drawer1Fragment, R.id.drawer2Fragment
+        )
+                .setOpenableLayout(binding.drawerLayout)
+                .build();
+
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration);
+    }
+}
